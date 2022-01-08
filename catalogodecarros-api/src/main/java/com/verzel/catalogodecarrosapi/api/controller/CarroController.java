dfb0ace3233487c0a15151cd0109442d38cce70d
robertocajueiro <beto.cajueiro@gmail.com>
2022-01-08 13:14:45 -0300

    criando as exception, service e controller

--- catalogodecarros-api/src/main/java/com/verzel/catalogodecarrosapi/api/controller/CarroController.java
+++ catalogodecarros-api/src/main/java/com/verzel/catalogodecarrosapi/api/controller/CarroController.java
@@ -1,10 +1,15 @@
 package com.verzel.catalogodecarrosapi.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.verzel.catalogodecarrosapi.domain.model.Carro;
 import com.verzel.catalogodecarrosapi.domain.repository.CarroRepository;
+import com.verzel.catalogodecarrosapi.domain.service.CadastroCarroService;
 
 @RestController
 @RequestMapping(value = "/carros")
@@ -12,6 +17,15 @@ public class CarroController {
 	
 	@Autowired
 	private CarroRepository carroRepository;
+	
+	@Autowired
+	private CadastroCarroService carroService;
+	
+	@GetMapping
+	public List<Carro> listar(){
+		return carroRepository.findAll();
+	}
+	
 	
 	
 	
--- catalogodecarros-api/src/main/java/com/verzel/catalogodecarrosapi/domain/service/CadastroCarroService.java
+++ catalogodecarros-api/src/main/java/com/verzel/catalogodecarrosapi/domain/service/CadastroCarroService.java
@@ -1,5 +1,43 @@
 package com.verzel.catalogodecarrosapi.domain.service;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.verzel.catalogodecarrosapi.domain.exception.EntidadeEmUsoException;
+import com.verzel.catalogodecarrosapi.domain.exception.EntidadeNaoEncontradaException;
+import com.verzel.catalogodecarrosapi.domain.model.Carro;
+import com.verzel.catalogodecarrosapi.domain.repository.CarroRepository;
+
+@Service
 public class CadastroCarroService {
+	
+	private static final String MSG_CARRO_EM_USO = 
+			"Carro de código %d não pode ser removida, pois está em uso";
+	
+	private static final String MSG_CARRO_NAO_ENCONTRADO = 
+			"Não existe um cadastro de carro com código %d";
+	
+	private CarroRepository carroRepository;
+	
+	public Carro salvar(Carro carro) {
+		return carroRepository.save(carro);
+	
+	}
+	
+	public void excluir(Long carroId) {
+		try {
+			carroRepository.deleteById(carroId);
+		} catch (EmptyResultDataAccessException e) {
+			
+			throw new EntidadeNaoEncontradaException(String.format(MSG_CARRO_NAO_ENCONTRADO, carroId));
+		
+		} catch(DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(
+					String.format(MSG_CARRO_EM_USO, carroId));
+		}
+	}
+	
+	
 
 }
